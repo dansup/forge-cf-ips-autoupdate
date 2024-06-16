fce4276ffc2f9741fda677d90947801e69a4d8ad
Daniel Supernault <danielsupernault@gmail.com>
2024-06-16 02:13:02 -0600

    Update cache config

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_STORE', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -102,6 +102,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
+    'prefix' => env('CACHE_PREFIX', 'pf_cf_ips_cache_'),
 
 ];
